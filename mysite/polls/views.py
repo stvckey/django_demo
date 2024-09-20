635808a534fb27e3ebfb79462863102ef6b7e2c3
stvckey <ettionestuckeyii@gmail.com>
2024-09-20 01:24:54 -0400

    We're basically finished with this tutorial. Good stuff!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
+from django.urls import reverse
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -12,13 +13,22 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -43,7 +53,12 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1 #The F just allows us to access and update the database.
         selected_choice.save()
+
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) #reverse() allows us to reconstruct the url from the name of the view and the arguments that need to be passed to it. This redirects to /polls/<question_id>/results/.
+        
+        # reverse() allows us to reconstruct the url from the name of the view and the arguments that need 
+        # to be passed to it. This redirects to /polls/<question_id>/results/.
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) 
